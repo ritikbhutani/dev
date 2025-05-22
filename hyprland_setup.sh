9b267179d5d2fa6e10bb1ff8548a72777ad4d3d2
Ritik Bhutani <code.rb9@gmail.com>
2025-05-23 01:02:58 +0530

    fix: refactor setup.sh

--- hyprland_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cp dotfiles/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
-cp dotfiles/waybar/config ~/.config/waybar/config
-cp dotfiles/hypr/hyprlock.conf ~/.config/hypr/hyprlock.conf
-cp dotfiles/hypr/hypridle.conf ~/.config/hypr/hypridle.conf
-
-sudo pacman -Su --needed --noconfirm wofi waybar ttf-font-awesome
-yay -S --noconfirm hyprshot hyprlock hypridle hyprpaper starship nwg-look
--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-export DEV=~/personal/dev
-
-sudo pacman -S --needed --noconfirm nautilus git base-devel
+sudo pacman -S --needed --noconfirm nautilus git base-devel stow
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
-rm -rf $DEV/yay-bin
+cd ..
+rm -rf ./yay-bin
 
-cp $DEV/.dotfiles/.bash_profile ~/.bash_profile
+sudo pacman -Su --needed --noconfirm wofi waybar ttf-font-awesome
+yay -Sy --noconfirm hyprshot hyprlock hypridle hyprpaper starship nwg-look brave-bin
 
-/bin/bash $DEV/hyprland_setup.sh
